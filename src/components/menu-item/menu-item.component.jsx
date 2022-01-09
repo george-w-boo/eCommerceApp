ba6f5adeda27ca0c934440bd281efce0b9d92387
Mykhailo Ostapysko <mp4clips@gmail.com>
2022-01-09 17:28:41 +0200

    adds routing

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
-
+import { withRouter } from "react-router";
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, linkUrl, history, match }) => {
+  console.log(history);
+  console.log(match);
+  console.log(linkUrl);
+
+  return (
+  <div
+    className={`${size} menu-item`}
+    onClick={() => {
+      history.push(`${match.url}${linkUrl}`)
+    }}
+  >
     <div
       className='background-image'
       style={{
@@ -15,6 +25,6 @@ const MenuItem = ({ title, imageUrl, size }) => (
       <span className='subtitle'>SHOP NOW</span>
     </div>
   </div>
-);
+)};
 
-export default MenuItem;
+export default withRouter(MenuItem);
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-
 import Directory from '../../components/directory/directory.component';
+import { Link } from "react-router-dom";
 
 import './homepage.styles.scss';
 
-const HomePage = () => (
+const HomePage = () => {
+
+  return (
   <div className='homepage'>
     <Directory />
   </div>
-);
+)};
 
 export default HomePage;
